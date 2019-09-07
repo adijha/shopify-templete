bceb39eabdc65c255ad397caf89a386802cd2290
Aditya Kumar Jha <adityajha126@gmail.com>
2019-09-08 02:37:25 +0530

    familiar

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-
-//init express app and bodyparser
 const app = express();
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  }).json()
-);
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
+
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'read_products';
+const forwardingAddress = 'https://b3ca6d9f.ngrok.io'; // Replace this with your HTTPS Forwarding address
 
 //send index.html file through response
-app.get('/', (request, response) =>
-  response.sendFile(`${__dirname}/frontend/index.html`)
-);
-
-// hearing post Request to this app
-app.post('/', (request, response) => {
-  const postBody = request.body.data;
-  console.log(postBody);
-
-  const text = new GoGo({
-    data: postBody
-  });
-  text.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      response.redirect('/');
-    }
-  });
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/frontend/index.html`)
 });
 
-//make post request to other endpoint (It's fake rest api implemetation to create employee data)
-app.post('http://dummy.restapiexample.com/api/v1/create', function(request, response) {
-  response.send({
-    id: '148862',
-    employee_name: 'mmm',
-    employee_salary: '111',
-    employee_age: '111',
-    profile_image: ''
-  });
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
 });
 
-//listen app on 9000 port
-app.listen(9000, () =>
-  console.info(
-    '==>🌎 Listening on port 3000. Open up http://localhost:9000/ in your browser 🎈🎈.'
-  )
-);
+
+app.listen(5000, () => {
+  console.log('Example app listening on port 3000!');
+});
